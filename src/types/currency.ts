b00d3ca3aeb88c2c46952083f622dd46407fca2d
AdamKorzun <a.korzun21@gmail.com>
2023-03-27 09:35:32 +0300

    feat - currency model

---
+++ src/types/currency.ts
@@ -0,0 +1,10 @@
+export interface ICurreny {
+  id: number;
+  name: string;
+  priceUSD: number;
+  change24h: string;
+  change1h: string;
+  change7d: string;
+  marketCap: number;
+  volume24h: number;
+}
