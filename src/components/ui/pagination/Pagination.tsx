7f4a18a4cb726fc853ab3399a54421b830c9a6f8
AdamKorzun <a.korzun21@gmail.com>
2023-03-27 15:38:51 +0300

    feat - pagination ui

---
+++ src/components/ui/pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '../buttons/Buttons';
+
+const Pagination = (): JSX.Element => {
+  return <Button text="show more" />;
+};
+
+export default Pagination;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CurrenciesList from '../../components/ui/currenciesList/CurrenciesList';
+import Pagination from '../../components/ui/pagination/Pagination';
 import styles from './Home.module.scss';
 const Home = (): JSX.Element => {
   return (
@@ -7,6 +8,9 @@ const Home = (): JSX.Element => {
       <main className={styles.main}>
         <h1 className={styles.title}>Top Currencies</h1>
         <CurrenciesList />
+        <div className={styles.paginationWrapper}>
+          <Pagination />
+        </div>
       </main>
     </>
   );
