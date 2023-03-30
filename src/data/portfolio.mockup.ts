70943bee32717a17e6222da1ac2b0e4e15cb0155
AdamKorzun <a.korzun21@gmail.com>
2023-03-30 15:01:17 +0300

    fix - removed data folder

--- src/data/portfolio.mockup.ts
+++
@@ -1,8 +0,0 @@
-import type { IPortfolio } from '../types/portfolio';
-import { currencies } from './currencies.mockup';
-export const portfolio: IPortfolio = {
-  assets: currencies.map((cur) => {
-    return { currency: cur, amount: 1 };
-  }),
-  totalValue: 0,
-};
