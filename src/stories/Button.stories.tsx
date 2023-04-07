e01fb3400b7b245531f43d3eff4c562709335105
AdamKorzun <a.korzun21@gmail.com>
2023-04-07 19:36:49 +0300

    feat - MultipleButtons story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,7 @@
 import { Button } from '../components/ui/buttons/Buttons';
 import type { Meta, StoryObj } from '@storybook/react';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
@@ -14,7 +15,7 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const MainButton: Story = {
+export const DefaultButton: Story = {
   args: {
     text: 'Button',
   },
@@ -38,5 +39,27 @@ export const WithIcon: Story = {
   args: {
     text: 'Button',
     icon: faMagnifyingGlass,
+  },
+};
+
+export const MultipleButtons: Story = {
+  render: (args) => (
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <Button {...args} tooltipText={undefined} icon={undefined} />
+      <Button
+        {...args}
+        disabled={true}
+        tooltipText={undefined}
+        icon={undefined}
+      />
+      <Button {...args} icon={undefined} />
+      <Button {...args} tooltipText={undefined} />
+    </div>
+  ),
+  args: {
+    text: DefaultButton.args?.text,
+    icon: WithIcon.args?.icon,
+    disabled: DisabledButton.args?.disabled,
+    tooltipText: HoverText.args?.tooltipText,
   },
 };
